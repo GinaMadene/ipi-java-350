ad5456f80ffe4d2c1ef03e2c1d67998854c3960d
GinaMadene <161767628+GinaMadene@users.noreply.github.com>
2025-11-01 20:04:20 +0000

    Update Employe.java

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -5,8 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.DayOfWeek;
-import java.time.LocalDate; // <-- Correction 2: Import manquant
-
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
@@ -15,7 +14,7 @@ public class Employe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id; // <-- Correction 1: Point-virgule ajouté
+    private Long id;
 
     private String nom;
 
@@ -61,18 +60,24 @@ public class Employe {
     }
 
     public Integer getNbRtt(LocalDate d){
-        int i1 = d.isLeapYear() ? 365 : 366;int var = 104;
+        // Correction : Remplacement de 'var' par 'nbRttJours' pour éviter l'identificateur restreint
+        int i1 = d.isLeapYear() ? 365 : 366;
+        int nbRttJours = 104;
+        
         switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
-        case THURSDAY: if(d.isLeapYear()) var =  var + 1; break;
+        case THURSDAY: if(d.isLeapYear()) nbRttJours = nbRttJours + 1; break;
         case FRIDAY:
-        if(d.isLeapYear()) var =  var + 2;
-        else var =  var + 1;
-        case SATURDAY:var = var + 1;
-                        break;
+        if(d.isLeapYear()) nbRttJours = nbRttJours + 2;
+        else nbRttJours = nbRttJours + 1;
+        case SATURDAY:nbRttJours = nbRttJours + 1;
+                         break;
         }
+        
         int monInt = (int) Entreprise.joursFeries(d).stream().filter(localDate ->
                 localDate.getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue()).count();
-        return (int) Math.ceil((i1 - Entreprise.NB_JOURS_MAX_FORFAIT - var - Entreprise.NB_CONGES_BASE - monInt) * tempsPartiel);
+                
+        // Utilisation de nbRttJours
+        return (int) Math.ceil((i1 - Entreprise.NB_JOURS_MAX_FORFAIT - nbRttJours - Entreprise.NB_CONGES_BASE - monInt) * tempsPartiel);
     }
 
     /**
